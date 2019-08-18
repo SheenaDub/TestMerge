b143743ff4e968c3125dad64b8eb06c63b0ecb29
Sheena Davitt <sheena.m.davitt@gm.com>
2019-08-18 19:12:29 +0100

    sheenadub2 commit with merge conflict

--- src/Test.java
+++ src/Test.java
@@ -6,5 +6,8 @@ public class Test {
 
         //This change added in branch v2
         System.out.println("changed in v2");
+
+        //This change added by SheenaDub2 github account
+        System.out.println("change by sheenadub2");
     }
 }
